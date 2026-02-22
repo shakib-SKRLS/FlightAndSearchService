95f3ce4e7fa8a125e5f5382c49b4651879740b7c
shakib-SKRLS <sk2724274@gmail.com>
2026-02-22 15:35:38 +0530

    completed city repo

--- src/repositories/city-repository.js
+++ src/repositories/city-repository.js
@@ -3,21 +3,44 @@ const {City} = require('../models/index')
 class CityRepository{
     async createCity({name}){
         try{
-            const city =await City.create({name});
-            return city;
+            const city =await City.create({
+                name: name
+            });
+           return city;
         } catch(error){
-            throw {error};
+            throw {"something went wrong in city repository layer": error};
         }
     }
     async deleteCity(cityId){
         try {
-            await City.distroy({
+            await City.destroy({
                 where: {
                     id: cityId
                 }
             })
+            return true;
         } catch (error) {
-            throw {error};
+            throw {"something went wrong in city repository layer": error};
+        }
+    }
+    async getCity(cityId){
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            throw {"something went wrong in city repository layer": error};
+        }
+    }
+    async updateCity(cityId, data){
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            throw {"something went wrong in city repository layer": error};
         }
     }
 }
