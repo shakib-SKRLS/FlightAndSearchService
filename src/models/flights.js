5687364e684e4d98a044427fb9b3492210741b41
shakib-SKRLS <sk2724274@gmail.com>
2026-04-05 15:04:36 +0530

    fixed the error in airport model and its migration

--- src/models/flights.js
+++ src/models/flights.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
   }
   Flights.init({
     flightNumber: {
-      type:DataTypes.STRING,
+      type:DataTypes.STRING, 
       allowNull: false,
       unique:true
     },
